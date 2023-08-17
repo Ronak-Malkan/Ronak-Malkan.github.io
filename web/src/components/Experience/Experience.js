4bdba44b1a0492bfa2ad69a5a406ba6473c7c710
Ronak-Malkan <ronakmalkan2062001@gmail.com>
2023-08-17 09:16:55 +0530

    feat: added responsiveness

--- web/src/components/Experience/Experience.js
+++ web/src/components/Experience/Experience.js
@@ -2,7 +2,7 @@ const Experience = () => {
    return (
       <div className="InfoContainer">
          <h3 className="title">Ardidra Technologies Pvt Ltd</h3>
-         <p>
+         <p className="whoAmI">
             I worked here as a Project Trainee for 4 months, from Jan 2023 to
             May 2023. In this time I leanred about Salesforce and it's various
             tools, such as: Apex, LWC, Visualforce, SOQL, etc. I created a URL
